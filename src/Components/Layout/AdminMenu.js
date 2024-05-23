64fbc557c7fd0d7107f9d02ddc0b9eb6a0517d19
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-05-23 15:38:30 +0530

    Create AdminMenu.js

---
+++ src/Components/Layout/AdminMenu.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const AdminMenu = () => {
+  return (
+    <>
+      <div className="text-centre">
+        <div className="list-group text-center">
+          <h4 className="text-center">Admin Panel</h4>
+          <Link to="/dashboard/admin/create-category" className="list-group-item list-group-item-action">
+            Create Category
+          </Link>
+          <Link to="/dashboard/admin/create-product" className="list-group-item list-group-item-action">
+            Create Products
+          </Link>
+          <Link to="/dashboard/admin/products" className="list-group-item list-group-item-action">
+            Products
+          </Link>
+          <Link to="/dashboard/admin/users" className="list-group-item list-group-item-action">
+            Users
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdminMenu;
