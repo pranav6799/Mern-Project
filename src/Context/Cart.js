6a6426c929598c5e5e7f7b6898aa076c933f5c8f
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-05-23 15:42:16 +0530

    Create Cart.js

---
+++ src/Context/Cart.js
@@ -0,0 +1,22 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const CartContext = createContext()
+
+const CartProvider = ({children})=>{
+  const[cart,setCart]= useState([])
+
+  useEffect(()=>{
+    let existingItem = localStorage.getItem('cart')
+    if(existingItem) setCart(JSON.parse(existingItem))
+  },[])
+return(
+  <CartContext.Provider value={[cart,setCart]}>
+    {children}
+  </CartContext.Provider>
+)
+}
+
+
+const useCart = ()=> useContext(CartContext)
+
+export {CartProvider,useCart}
