b188a32d401cefe5d45399331b647682509807ae
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-05-23 15:36:32 +0530

    Create CategoryForm.js

---
+++ src/Components/Form/CategoryForm.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const CategoryForm = ({value,handleSubmit,setValue}) => {
+ 
+
+  return (
+   <>
+   <form onSubmit={handleSubmit}>
+  <div className="mb-3">
+    <input type="text" className="form-control" placeholder='Enter New Category' value={value} onChange={(e)=> setValue(e.target.value)} />
+  </div>
+  
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+
+   </>
+  )
+}
+
+export default CategoryForm
+
