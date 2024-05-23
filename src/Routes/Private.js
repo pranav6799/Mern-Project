3d6ccedc8a11b0457fbc22456b66f0a667b59375
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-05-23 15:52:59 +0530

    Create Private.js

---
+++ src/Routes/Private.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import { useAuth } from '../Context/Auth'
+import { Outlet } from 'react-router-dom'
+import axios from 'axios'
+import Spinner from '../Components/Spinner'
+
+
+const PrivateRoute = () => {
+  const [ok,setOk] = useState(false)
+  const [auth]= useAuth()
+useEffect(()=>{
+  const authCheck =async()=>{
+      const response = await axios('http://localhost:8080/api/v1/auth/user-auth',{
+        headers:{
+          'Authorization':auth?.token
+        }
+      })
+      if(response.data.ok){
+        setOk(true)
+      }else{
+        setOk(false)
+      }
+  }
+  if(auth?.token){
+    authCheck()
+  }
+
+},[auth?.token])
+
+return ok ? <Outlet/> : <Spinner/>
+}
+
+export default PrivateRoute
