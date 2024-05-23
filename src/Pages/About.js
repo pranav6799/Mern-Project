4988d0d68bab3208550cdfa692c71d4918540bbc
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-05-23 15:48:55 +0530

    Create About.js

---
+++ src/Pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../Components/Layout/Layout'
+
+const About = () => {
+  return (
+    <Layout title={'About-Ecommerce'}>
+      <h1>About</h1>
+    </Layout>
+  )
+}
+
+export default About
