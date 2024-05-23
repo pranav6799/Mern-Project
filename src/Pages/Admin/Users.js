972b5c04c1d1b40482df2dbe6e0ad8f35f500a43
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-05-23 15:45:26 +0530

    Create Users.js

---
+++ src/Pages/Admin/Users.js
@@ -0,0 +1,22 @@
+import React from "react";
+import AdminMenu from "../../Components/Layout/AdminMenu";
+import Layout from "../../Components/Layout/Layout";
+
+const Users = () => {
+  return (
+    <Layout title={'Dashboard-AllUsers'}>
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu></AdminMenu>
+          </div>
+          <div className="col-md-9">
+            <h1>All Users</h1>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Users;
